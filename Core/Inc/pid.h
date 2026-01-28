c8ca5b5ef78e20427838421238776c5e925fcda9
miyeon <my3104306919@outlook.com>
2026-01-28 15:50:04 +0800

    codexv1加pid

--- Core/Inc/pid.h
+++ Core/Inc/pid.h
@@ -7,19 +7,24 @@
 
 typedef struct
 {
-    float targer_val;
+    float target_val;
     float actual_val;
     float err ;
     float err_last;
     float err_sum;
     float kp, ki, kd;
     float output_val;
+    float max_output;
+    float max_iout;
+    float d_filter_alpha;
+    float d_out;
 }tpid;
 
 float P_realize(tpid * pid,float actual_val);
 void PID_init(void);
 float PI_realize(tpid * pid,float actual_val);
 float PID_realize(tpid * pid,float actual_val);
+void PID_Reset(tpid * pid);
 
 
 #endif //CAR_PID_H
--- Core/Src/pid.c
+++ Core/Src/pid.c
@@ -15,9 +15,9 @@ tpid k210motion;
 
 void PID_init(void)
 {
-    //输入0到10，输出0-100；
+    //输入0�?0，输�?-100�?
     pidMotor1Speed.actual_val=0.0;
-    pidMotor1Speed.targer_val=0.0;
+    pidMotor1Speed.target_val=0.0;
     pidMotor1Speed.output_val=0.0;
     pidMotor1Speed.err=0.0;
     pidMotor1Speed.err_last=0.0;
@@ -25,9 +25,13 @@ void PID_init(void)
     pidMotor1Speed.kd=0.0;
     pidMotor1Speed.ki=0.1;
     pidMotor1Speed.kp=8.0;
+    pidMotor1Speed.max_output=100.0;
+    pidMotor1Speed.max_iout=500.0;
+    pidMotor1Speed.d_filter_alpha=0.2;
+    pidMotor1Speed.d_out=0.0;
 
     pidMotor2Speed.actual_val=0.0;
-    pidMotor2Speed.targer_val=0.0;
+    pidMotor2Speed.target_val=0.0;
     pidMotor2Speed.output_val=0.0;
     pidMotor2Speed.err=0.0;
     pidMotor2Speed.err_last=0.0;
@@ -35,9 +39,13 @@ void PID_init(void)
     pidMotor2Speed.kd=0.0;
     pidMotor2Speed.ki=0.1;
     pidMotor2Speed.kp=8.0;
+    pidMotor2Speed.max_output=100.0;
+    pidMotor2Speed.max_iout=500.0;
+    pidMotor2Speed.d_filter_alpha=0.2;
+    pidMotor2Speed.d_out=0.0;
 
     pid_pidHW_Tracking.actual_val=0.0;
-    pid_pidHW_Tracking.targer_val=0.0;
+    pid_pidHW_Tracking.target_val=0.0;
     pid_pidHW_Tracking.output_val=0.0;
     pid_pidHW_Tracking.err=0.0;
     pid_pidHW_Tracking.err_last=0.0;
@@ -45,35 +53,54 @@ void PID_init(void)
     pid_pidHW_Tracking.kd=1;
     pid_pidHW_Tracking.ki=0.0;
     pid_pidHW_Tracking.kp=-2;
+    pid_pidHW_Tracking.max_output=100.0;
+    pid_pidHW_Tracking.max_iout=500.0;
+    pid_pidHW_Tracking.d_filter_alpha=0.2;
+    pid_pidHW_Tracking.d_out=0.0;
 
 
     pidFollow.actual_val=0.0;
-    pidFollow.targer_val=22.50;//定距离跟随 目标距离22.5cm
+    pidFollow.target_val=22.50;
+    pidFollow.output_val=0.0;
     pidFollow.err=0.0;
     pidFollow.err_last=0.0;
     pidFollow.err_sum=0.0;
-    pidFollow.kp=-0.5;//定距离跟随的Kp大小通过估算PID输入输出数据，确定大概大小，然后在调试
-    pidFollow.ki=-0.001;//Ki小一些
+    pidFollow.kp=-0.5;
+    pidFollow.ki=-0.001;
     pidFollow.kd=0;
+    pidFollow.max_output=100.0;
+    pidFollow.max_iout=500.0;
+    pidFollow.d_filter_alpha=0.2;
+    pidFollow.d_out=0.0;
 
     mpu6050Movement.actual_val=0.0;
-    mpu6050Movement.targer_val=0.0;
+    mpu6050Movement.target_val=0.0;
+    mpu6050Movement.output_val=0.0;
     mpu6050Movement.err=0.0;
     mpu6050Movement.err_last=0.0;
     mpu6050Movement.err_sum=0.0;
     mpu6050Movement.kd=0.1;
     mpu6050Movement.ki=0.0;
     mpu6050Movement.kp=0.02;
+    mpu6050Movement.max_output=100.0;
+    mpu6050Movement.max_iout=500.0;
+    mpu6050Movement.d_filter_alpha=0.2;
+    mpu6050Movement.d_out=0.0;
 
 
     k210motion.actual_val=0.0;
-    k210motion.targer_val=160.0;
+    k210motion.target_val=160.0;
+    k210motion.output_val=0.0;
     k210motion.err=0.0;
     k210motion.err_last=0.0;
     k210motion.err_sum=0.0;
     k210motion.kd=0.1;
     k210motion.ki=0.0;
     k210motion.kp=0.02;
+    k210motion.max_output=100.0;
+    k210motion.max_iout=500.0;
+    k210motion.d_filter_alpha=0.2;
+    k210motion.d_out=0.0;
 
 }
 
@@ -81,7 +108,7 @@ void PID_init(void)
 float P_realize(tpid * pid,float actual_val)
 {
     pid->actual_val=actual_val;
-    pid->err = pid->targer_val - pid->actual_val;
+    pid->err = pid->target_val - pid->actual_val;
     pid->actual_val = pid->kp * pid->err ;
 
     return pid->actual_val;
@@ -92,7 +119,7 @@ float P_realize(tpid * pid,float actual_val)
 float PI_realize(tpid * pid,float actual_val)
 {
     pid->actual_val=actual_val;
-    pid->err = pid->targer_val - pid->actual_val;
+    pid->err = pid->target_val - pid->actual_val;
     pid->err_sum += pid->err;
     pid->actual_val = pid->kp * pid->err + pid->ki * pid->err_sum;
     return pid->actual_val;
@@ -100,24 +127,54 @@ float PI_realize(tpid * pid,float actual_val)
 
 float PID_realize(tpid * pid,float actual_val)
 {
+    float d_raw;
+    float i_candidate;
+    float output_raw;
+    float output_limited;
+    int saturating;
+
     pid->actual_val = actual_val;
-    pid->err = pid->targer_val - pid->actual_val;
-    pid->err_sum += pid->err;
+    pid->err = pid->target_val - pid->actual_val;
 
-    if(pid->err_sum > 500)  pid->err_sum = 500;
-    if(pid->err_sum < -500) pid->err_sum = -500;
+    i_candidate = pid->err_sum + pid->err;
+    if (pid->max_iout > 0.0)
+    {
+        if (i_candidate > pid->max_iout) i_candidate = pid->max_iout;
+        if (i_candidate < -pid->max_iout) i_candidate = -pid->max_iout;
+    }
 
+    d_raw = pid->err - pid->err_last;
+    pid->d_out += pid->d_filter_alpha * (d_raw - pid->d_out);
 
-    pid->output_val = pid->kp * pid->err + pid->ki * pid->err_sum + pid->kd * (pid->err-pid->err_last);
+    output_raw = pid->kp * pid->err + pid->ki * i_candidate + pid->kd * pid->d_out;
+    output_limited = output_raw;
+    if (pid->max_output > 0.0)
+    {
+        if (output_limited > pid->max_output) output_limited = pid->max_output;
+        if (output_limited < -pid->max_output) output_limited = -pid->max_output;
+    }
 
+    saturating = (pid->max_output > 0.0) && (output_limited != output_raw);
+    if (!(saturating && ((output_raw > pid->max_output && pid->err > 0.0) ||
+        (output_raw < -pid->max_output && pid->err < 0.0))))
+    {
+        pid->err_sum = i_candidate;
+    }
+
+    pid->output_val = output_limited;
     pid->err_last = pid->err;
-
-
-    if(pid->output_val > 100) pid->output_val = 100;
-    if(pid->output_val < -100) pid->output_val = -100;
-
 
     return pid->output_val;
 
+}
+
+void PID_Reset(tpid * pid)
+{
+    pid->actual_val = 0.0;
+    pid->err = 0.0;
+    pid->err_last = 0.0;
+    pid->err_sum = 0.0;
+    pid->d_out = 0.0;
+    pid->output_val = 0.0;
 }
 
